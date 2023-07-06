cb4294631233420e8b7eca2879665662c8d742f8
amisha <amishaadwani0023@gmail.com>
2023-07-06 23:46:41 +0530

    new commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-jjjj
+
+const apikey= '';
+const url ='https://api.openweathermap.org/data/2.5/weather?q=berlin&units=metric'
+
+
+
+document.getElementById('submit').onclick= async function(){
+    const response = await fetch (url+ `&appid=${apikey}`)
+    let data = await response.json()
+    console.log(data.weather)
+    let data2 = data.weather[0].main;
+    document.getElementById('display').innerText = data2
+}
