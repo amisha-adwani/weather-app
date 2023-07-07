05ddcfa7038b46608578e1866d00c34902efc3a0
amisha <amishaadwani0023@gmail.com>
2023-07-07 19:23:40 +0530

    updated commit

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
-const apikey= '';
-const url ='https://api.openweathermap.org/data/2.5/weather?q=berlin&units=metric'
 
 
-
-document.getElementById('submit').onclick= async function(){
-    const response = await fetch (url+ `&appid=${apikey}`)
+document.getElementById('submit').onclick = async function(){
+    let city = document.getElementById('city')
+    let cityValue = city.value
+    const apikey= '';
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=metric`
+    const response = await fetch (url+`&appid=${apikey}` )
     let data = await response.json()
-    console.log(data.weather)
-    let data2 = data.weather[0].main;
-    document.getElementById('display').innerText = data2
+    console.log(data)
+    let data2 = data.weather[0].description;
+    let data3 = data.main.temp
+    document.getElementById('display').innerText = data2+" "+ data3+'°C'
+    console.log(data2,data3)
 }
