76192aff64864e50f894578e653c7303e90324ec
amisha <amishaadwani0023@gmail.com>
2023-07-07 20:38:11 +0530

    updates

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-
-
-
-document.getElementById('submit').onclick = async function(){
-    let city = document.getElementById('city')
-    let cityValue = city.value
-    const apikey= '';
-    const url =`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=metric`
-    const response = await fetch (url+`&appid=${apikey}` )
-    let data = await response.json()
-    console.log(data)
-    let data2 = data.weather[0].description;
-    let data3 = data.main.temp
-    document.getElementById('display').innerText = data2+" "+ data3+'°C'
-    console.log(data2,data3)
-}
+document.getElementById("submit").onclick = async function () {
+  let city = document.getElementById("city");
+  let cityValue = city.value;
+  const apikey = "";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=metric`;
+  const response = await fetch(url + `&appid=${apikey}`);
+  let data = await response.json();
+  let data2 = data.weather[0].description;
+  let data3 = data.main.temp;
+  let data4 = data.name;
+  let data5 = data.sys.country;
+  document.getElementById("displayDeg").innerText = data3 + "°C";
+  document.getElementById("displayDesc").innerText = data2;
+  document.getElementById("displayCity").innerText = data4 + ", " + data5;
+};
